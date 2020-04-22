dfb0f655ba618221a33defe1993f4ec91b4d783c
atsuya-koshida <koshi0845@gmail.com>
2020-04-22 15:02:38 +0900

    ユーザー編集画面を実装

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
+
 Route::group(['prefix' => 'users', 'middleware' => 'auth'], function () {
     Route::get('show/{id}', 'UserController@show')->name('users.show');
+    Route::get('edit/{id}', 'UserController@edit')->name('users.edit');
+    Route::post('update/{id}', 'UserController@update')->name('users.update');
 });
 
 Auth::routes();
